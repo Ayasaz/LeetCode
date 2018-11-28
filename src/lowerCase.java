b2658610d701bab3127b57ec391232c0d78cfab1
Ayasaz <33181051+Ayasaz@users.noreply.github.com>
2018-11-28 12:08:39 +0800

    Initial commit

---
+++ src/lowerCase.java
@@ -0,0 +1,11 @@
+public class lowerCase {
+    public static void main(String args[]){
+        String a = "Hello";
+        String b = toLowerCase(a);
+        System.out.println(b);
+    }
+    public static String toLowerCase(String str){
+        String s1 = str.toLowerCase();
+        return  s1;
+    }
+}
